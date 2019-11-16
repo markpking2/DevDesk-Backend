402c69045e487d3372fac3495c17a187b1a1b1db
Mark King <markpkng@gmail.com>
2019-11-16 00:14:07 -0500

    users model, cleanup, migrations

---
+++ data/migrations/20191116001027_resolved_tickets.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('resolved_tickets', tbl => {
+        tbl.increments();
+        tbl.integer('student_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        tbl.integer('helper_id')
+            .unsigned()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        tbl.integer('ticket_id')
+            .unsigned()
+            .notNullable()
+            .unique()
+            .references('id')
+            .inTable('tickets')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        tbl.timestamp('resolved_at')
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        tbl.unique(['helper_id', 'student_id']);
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('resolved_tickets');
+};
