ebc2e2caaeabb747236bc78b26c2040eff1fa6f2
Mark King <markpkng@gmail.com>
2019-11-17 19:39:12 -0500

    slackbot

--- slackbot.js
+++ slackbot.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const axios = require('axios');
+const request = require('request');
 
 
 router.post('/', (req, res) => {
@@ -8,7 +9,7 @@ router.post('/', (req, res) => {
           channel: "#general",
           text: "Hi! :wave: \n I am here to destroy all humans. Thank you for creating me."
         }};
-    axios.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+    request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
           // Sends welcome message
           res.json();
         });
